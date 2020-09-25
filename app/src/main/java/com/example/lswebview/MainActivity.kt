a4e0dc8deace26f982fb136840bcd1fe2e04f761
liangshan.xiong <liangshan.xiong@zhangmen.com>
2020-09-25 11:24:29 +0800

    删除无用代码

--- app/src/main/java/com/example/lswebview/MainActivity.kt
+++ app/src/main/java/com/example/lswebview/MainActivity.kt
@@ -9,7 +9,6 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
 
     companion object{
-        const val url1 = "https://test.hdkj.zmlearn.com/zmpark/web-mobile/index.html?device=aPad&msgSendModle=jsb&env=test&userId=1326319398&token=eyJhbGciOiJSUzI1NiJ9.eyJqdGkiOiJmWVhuSlZwYVZMd1I0VmJWVnRESnVBPT0iLCJmcm9tIjoiS2lkc1BhZCIsIm5iZiI6MTYwMDgzMDk4OX0.OepxWJ48rYuYlkt3BA6OZJZmSC2iimfkY6W6JAy37ZRz3b1ayABsLyDCBukvUBqrEXXCS3-PBg-I58mEjPUxsKQ9m9uosDIhdZhhjWhAMuxhsK4CDOmA-FBiiuQcnn4WcNqDSS_6_pIUSah0sfzpTc9B_LNR3u_bvgqq-qVaNGg"
         const val url2 = "https://www.baidu.com"
     }
     override fun onCreate(savedInstanceState: Bundle?) {
